7ef402c4d8c2062ea2333cc9235276621c7720fe
katenev <katenev.ar@yandex.ru>
2026-03-03 17:36:44 +0300

    wp

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import { useEffect } from "react";
+import { getAuth } from "firebase/auth";
+import app from "./firebase";
 
 function App() {
+
+  useEffect(() => {
+    const auth = getAuth(app);
+    console.log("Auth instance:", auth);
+  }, []);
 
   return (
     <div>
       auth-react
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
